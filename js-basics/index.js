8a0267837ed1149677ace8b3f377d804b0350870
oluwaseun007 <preciousopal.od@gmail.com>
2023-02-14 16:31:50 +0100

    console log JS

--- js-basics/index.js
+++ js-basics/index.js
@@ -3,3 +3,11 @@ function multie(value1, value2) {
 }
 console.log(multie(7, 3));
 
+let firstN = "Dorcas";
+let lastN = "Opatola";
+let StuternTrack = "Backend developer";
+let skill = "c, python and javascript";
+let colour = "black";
+let independent_ = "1960";
+
+console.log("My name is",firstN, lastN,." I am a",StuternTrack,", with indepth knowledge of industry skills such as",skill,". My go-to colour is",colour,". In the year",independent_,", Nigeria got her independence.");
