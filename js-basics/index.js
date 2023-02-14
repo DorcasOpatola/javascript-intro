d82770e521555b89d0e4bab9498b4122613107df
oluwaseun007 <preciousopal.od@gmail.com>
2023-02-14 16:43:54 +0100

    console log JS

--- js-basics/index.js
+++ js-basics/index.js
@@ -10,4 +10,4 @@ let skill = "c, python and javascript";
 let colour = "black";
 let independent_ = "1960";
 
-console.log("My name is",firstN, lastN,." I am a",StuternTrack,", with indepth knowledge of industry skills such as",skill,". My go-to colour is",colour,". In the year",independent_,", Nigeria got her independence.");
+console.log("My name is",firstN, lastN,". I am a",StuternTrack,", with indepth knowledge of industry skills such as",skill,". My go-to colour is",colour,". In the year",independent_,", Nigeria got her independence.");
