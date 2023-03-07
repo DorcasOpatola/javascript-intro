839918e07d1bc734fc92c74fcb4bbc37a2860002
oluwaseun007 <preciousopal.od@gmail.com>
2023-03-07 10:56:15 +0100

    js arrays

---
+++ arrays-functions/index.js
@@ -0,0 +1,15 @@
+// 1.   MUTATING ARRAY METHOD is used to add, remove or replace items directly to an array, such that the
+//          original array dataset gets permanently altered.
+//          Examples: array.push(), array.unshift(), array.splice(), array.pop(), array.shift() .
+
+//      NON-MUTATING ARRAY METHOD is used to add, remove, or replace items in an array, in such a way that 
+//          the original array dataset remains unaltered. 
+//          Examples: array.slice(), array.filter(), array.concat(), ...array, array.map() .
+
+
+
+let languages = ['C#', 'JavaScript', 'Ruby', 'PHP', 'Python'];
+
+// 2.   Add ‘Kotlin’ to the end of the array
+languages.push('Kotlin');
+console.log(languages);
