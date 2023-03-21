755cbbf23e0666748a559c0ac09f74ba6c24a1a2
oluwaseun007 <preciousopal.od@gmail.com>
2023-03-21 03:04:54 +0100

    an array of numbers

--- arrays-functions/index.js
+++ arrays-functions/index.js
@@ -11,5 +11,26 @@
 let languages = ['C#', 'JavaScript', 'Ruby', 'PHP', 'Python'];
 
 // 2.   Add ‘Kotlin’ to the end of the array
-languages.push('Kotlin');
-console.log(languages);
+languages.push('Kotlin') // languages = ['C#', 'JavaScript', 'Ruby', 'PHP', 'Python', 'Kotlin']
+
+//3.    fruit = ['apple', 'mango', 'orange']
+
+
+//4.    
+function max(numbers) {
+    let maximum = numbers[0]; // assume the first element is the maximum
+
+    for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] > maximum) {
+        maximum = numbers[i];
+    }}
+
+    return maximum;
+}
+    
+
+//5. 
+function valTimesIndex(arr) {
+    return arr.map((value, index) => value * index);
+  }
+  
