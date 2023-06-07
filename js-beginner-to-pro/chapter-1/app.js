43eba8bf91ad11d7a884900fa6a354e09ae5b80f
oluwaseun007 <preciousopal.od@gmail.com>
2023-06-07 09:51:16 +0100

    how to write a multi-line comment

--- js-beginner-to-pro/chapter-1/app.js
+++ js-beginner-to-pro/chapter-1/app.js
@@ -2,3 +2,10 @@ console.log("Say my name...");
 
 let a = 10;     // Assigned a value of 10 to variable 'a'.
 console.log(a); // Logs out the value of 'a'.
+
+/*
+    A multi-line comment
+    in JavaScript 
+    looks like
+    this.
+*/
