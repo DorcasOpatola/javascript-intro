456ccd5bed190fb3137831f82936c4e246759a92
oluwaseun007 <preciousopal.od@gmail.com>
2023-07-17 20:43:39 +0100

    data type operators

---
+++ devcareer/data-types/index.js
@@ -0,0 +1,14 @@
+What does the following code print?
+
+
+let name = "Ilya";
+
+// the expression is a number
+alert( `hello ${1}` ); // hello 1
+
+// the expression is a string "name"
+alert( `hello ${"name"}` ); // hello name
+
+// the expression is a variable, embed it
+alert( `hello ${name}` ); // hello Ilya
+
