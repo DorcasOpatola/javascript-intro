ce1b0007d4bb824d9f91ba83ad27fdf5ddbec8fd
oluwaseun007 <preciousopal.od@gmail.com>
2023-07-21 00:46:30 +0100

    alert, prompts

---
+++ devcareer/interaction/alert-confirm-prompt.js
@@ -0,0 +1,11 @@
+"use strict";
+
+
+let firstName = "Verath";
+let lastName = "Dami";
+
+alert("Hi " + firstName + lastName);
+
+let age = prompt("How old are you?", 0);
+
+alert(`You do amazing things for someone merely ${age} years old! I am impressed!`);
