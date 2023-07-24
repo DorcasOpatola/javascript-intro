86aaecbb18e36d1e1626bf67bb3e9d6c1899a907
oluwaseun007 <preciousopal.od@gmail.com>
2023-07-24 07:42:41 +0100

    using prompt function

--- devcareer/alert-confirm-prompt.js
+++ devcareer/alert-confirm-prompt.js
@@ -8,5 +8,7 @@ alert("Hi " + firstName +" " + lastName);
 
 let age = prompt("How old are you " + firstName + "?", 0);
 
-alert(`You do amazing things for someone merely ${age} years of age ${firstName}! I am impressed!`
-);
+alert(`You do amazing things for someone merely ${age} years of age ${firstName}! I am impressed!`);
+
+firstName = prompt("What is your name?", "First Name");
+alert(`Welcome to Nigeria ${firstName}, we are delighted to have you!`);
